87732cc49abf6cb38da1f04b96332e2bd8d3272e
RaiyanJiyon <raiyanjiyon814@gmail.com>
2024-12-10 18:28:31 +0600

    Set up main application structure
    - Added main entry point in main.jsx with React and ReactDOM configuration
    - Implemented router setup in Routes.jsx using react-router-dom
    - Created MainLayout, Home, AllVisas, AddVisa, MyAddedVisas, MyApplications, Login, Register, VisaDetails, and NotFound pages
    - Set up PrivateRoute for protected routes
    - Configured RouterProvider to use the created routes

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,7 +1,7 @@
 const MainLayout = () => {
     return (
         <div>
-            
+
         </div>
     );
 };
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,9 +1,72 @@
-const Routes = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout";
+import Home from "../pages/Home";
+import AllVisas from "../pages/AllVisas";
+import AddVisa from "../pages/AddVisa";
+import MyAddedVisas from "../pages/MyAddedVisas";
+import MyApplications from "../pages/MyApplications";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import VisaDetails from "../pages/VisaDetails";
+import NotFound from "../pages/NotFound";
+import PrivateRoute from "./PrivateRoute";
 
-export default Routes;
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <MainLayout />,
+        errorElement: <NotFound />,
+        children: [
+            {
+                path: "/",
+                element: <Home />,
+            },
+            {
+                path: "/all-visas",
+                element: <AllVisas />,
+            },
+            {
+                path: "/visa/:id",
+                element: (
+                    <PrivateRoute>
+                        <VisaDetails />
+                    </PrivateRoute>
+                ),
+            },
+            {
+                path: "/add-visa",
+                element: (
+                    <PrivateRoute>
+                        <AddVisa />
+                    </PrivateRoute>
+                ),
+            },
+            {
+                path: "/my-added-visas",
+                element: (
+                    <PrivateRoute>
+                        <MyAddedVisas />
+                    </PrivateRoute>
+                ),
+            },
+            {
+                path: "/my-applications",
+                element: (
+                    <PrivateRoute>
+                        <MyApplications />
+                    </PrivateRoute>
+                ),
+            },
+            {
+                path: "/login",
+                element: <Login />,
+            },
+            {
+                path: "/register",
+                element: <Register />,
+            },
+        ],
+    },
+]);
+
+export default router;
