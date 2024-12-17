f197c4c57d8f8c0b7b6b4e8d8750a9b95ea68616
RaiyanJiyon <raiyanjiyon814@gmail.com>
2024-12-17 20:58:38 +0600

    Implement sorting functionality in AllVisas component
    - Added state for selected filter type
    - Implemented filter logic to filter visas based on the selected filter
    - Updated dropdown menu to allow users to select different visa types
    - Ensured VisaCard component displays the filtered visa details

--- src/pages/AllVisas.jsx
+++ src/pages/AllVisas.jsx
@@ -1,17 +1,39 @@
-import { Link, useLoaderData } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
+import { useState } from "react";
 import VisaCard from "../components/visa/VisaCard";
 
 const AllVisas = () => {
     const allVisas = useLoaderData();
+    const [filter, setFilter] = useState('All Visas');
+
+    const handleFilterChange = (filterType) => {
+        setFilter(filterType);
+    };
+
+    const filteredVisas = filter === 'All Visas' ? allVisas : allVisas.filter(visa => visa.Visa_Type === filter);
+
     return (
-        <div className="">
-            <div className="flex flex-col gap-2 py-10 bg-blue-500 text-white text-center">
-                <h2 className="text-3xl font-bold">All Visas</h2>
-                <p className="font-medium">Visa has developed a programme that helps confirm your identity when you make an online purchase.</p>
+        <div className="w-11/12 mx-auto">
+            <div className="mt-10 mb-4">
+                <h2 className="text-3xl font-bold text-center">
+                    Visa Application Management
+                </h2>
+                <p className="font-medium text-center">
+                    View and manage the status of your visa applications with ease.
+                </p>
             </div>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 w-11/12 mx-auto mt-10">
+            <div className="dropdown dropdown-hover">
+                <div tabIndex={0} role="button" className="btn m-1 bg-sky-600 text-white font-bold">Filtered By</div>
+                <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+                    <li><a onClick={() => handleFilterChange('All Visas')}>All Visas</a></li>
+                    <li><a onClick={() => handleFilterChange('Tourist Visa')}>Tourist Visa</a></li>
+                    <li><a onClick={() => handleFilterChange('Student Visa')}>Student Visa</a></li>
+                    <li><a onClick={() => handleFilterChange('Official Visa')}>Official Visa</a></li>
+                </ul>
+            </div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-4">
                 {
-                    allVisas.map((visa, idx) => (
+                    filteredVisas.map((visa, idx) => (
                         <VisaCard key={idx} visa={visa} />
                     ))
                 }
